2ff7aa25ec3f4b99b83499298afd937f8e51c88f
Cha1mae <chaimastaifa@gmail.com>
2024-01-17 10:42:44 -0800

    error pycode fixed

--- models/state.py
+++ models/state.py
@@ -4,14 +4,23 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship(
+        "City", backref="state", cascade="all, delete-orphan"
+    )
 
     def to_dict(self):
         """wa kanghwt wa 3yit"""
         dict_representation = super().to_dict()
         dict_representation['__class__'] = self.__class__.__name__
         return dict_representation
+
+    def __str__(self):
+        """ i added this in hopes of winning"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.to_dict()
+        )
