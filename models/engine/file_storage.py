6ef03cbb54c8e61e521326d9b08046041aa3cdd4
Cha1mae <chaimastaifa@gmail.com>
2024-01-17 07:08:31 -0800

    fixed my pycodeerrors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,21 +8,23 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None)
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
-            return FileStorage.__objects
-	else:
-	    filtered_objects = {
-            key: val for key, val in FileStorage.__objects.items() if isinstance(val, cls)}
-            return filtered_objects
+    if cls is None:
+        return FileStorage.__objects
+    else:
+        filtered_objects = {
+            key: val for key, val in FileStorage.__objects.items()
+            if isinstance(val, cls)
+        }
+        return filtered_objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """Adds a new object to the storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """Saves the storage dictionary to a file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -31,7 +33,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Loads the storage dictionary from a file"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -41,10 +43,10 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -55,14 +57,13 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Delete a given object if it exists."""
+        """Deletes a given object if it exists."""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
                 self.save()
 
-
     def close(self):
-        """Call the reload method."""
+        """Calls the reload method."""
         self.reload()
