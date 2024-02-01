fc316d3804c82f072d4a24c9352950c1b3dc6bab
Cha1mae <chaimastaifa@gmail.com>
2024-02-01 06:16:58 -0800

    crying

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -32,6 +32,28 @@ def do_pack():
         return None
 
 
+def do_pack():
+    """
+    Generates a .tgz archive from the web_static folder content
+
+    Returns:
+        str: Archive path or None otherwise.
+    """
+    local("mkdir -p versions")
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    ap = f"versions/web_static_{timestamp}.tgz"
+    ac = f"tar -cvzf {ap} web_static"
+    result = local(ac)
+
+    if result.failed:
+        return None
+    else:
+        output = f"web_static packed: {ap} -> "
+        output += f"{result.stdout.split()[-1]}Bytes"
+        print(output)
+        return ap
+
+
 def do_deploy(archive_path):
     """
     Distributes an archive to my WS (web server)
