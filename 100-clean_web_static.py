3c663359cdcfc5fc2248108a19d4d1c7a7710f4a
Cha1mae <chaimastaifa@gmail.com>
2024-02-01 06:33:58 -0800

    iwa hadalah

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,7 +4,6 @@ Fabric script that deletes out-of-date archives
 """
 
 from fabric.api import env, local, run
-from datetime import datetime
 from fabric.decorators import runs_once
 
 env.hosts = ['<52.91.152.110>', '<52.87.152.252>']
@@ -28,11 +27,18 @@ def do_clean(number=0):
             return
 
         # Local cleanup on the machine
-        local("ls -t versions | tail -n +{} | xargs -I {{}} rm versions/{{}}".format(number + 1))
+        local_command = (
+            "ls -t versions | tail -n +{} | xargs -I {{}} rm versions/{{}}"
+        ).format(number + 1)
+        local(local_command)
 
-        # Remote cleanup on the  web servers
-        run("ls -t /data/web_static/releases | tail -n +{} | xargs -I {{}} rm -rf /data/web_static/releases/{{}}"
-            .format(number + 1))
+        # Remote cleanup on the web servers
+        remote_command = (
+            "ls -t /data/web_static/releases | "
+            "tail -n +{} | xargs -I {{}} rm -rf /data/web_static/releases/{{}}"
+        ).format(number + 1)
+
+        run(remote_command)
     except Exception as e:
         print(e)
 
